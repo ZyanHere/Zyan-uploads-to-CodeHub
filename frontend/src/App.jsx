0908350b8a27494872553203c46a792a3254b3a7
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-18 00:35:19 +0530

    app.jsx and sidebar component updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import LikesPage from "./pages/LikesPage";
+import ExplorePage from "./pages/ExplorePage";
 
 function App() {
   return (
-    <div>
-      <h2>hello</h2>
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5x1 my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
